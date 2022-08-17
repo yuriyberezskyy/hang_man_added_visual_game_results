d1c7dbaaede9493127e527a2fa0d343e66b7b95d
Yuriy Berezskyy <54465428+yuriyberezskyy@users.noreply.github.com>
2022-08-17 16:58:23 +0000

    new_files_and_new_UI

--- main.py
+++ main.py
@@ -1,71 +1,16 @@
 #Step 4
-
+import hangman_logo
+import hangman_words
 import random
 
-stages = ['''
-  +---+
-  |   |
-  O   |
- /|\  |
- / \  |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
- /|\  |
- /    |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
- /|\  |
-      |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
- /|   |
-      |
-      |
-=========''', '''
-  +---+
-  |   |
-  O   |
-  |   |
-      |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
-      |
-      |
-      |
-=========
-''', '''
-  +---+
-  |   |
-      |
-      |
-      |
-      |
-=========
-''']
+stages = hangman_logo.stages
+print(hangman_logo.logo)
+print("YOU HAVE 6 LIVES")
 
 end_of_game = False
-word_list = ["ardvark", "baboon", "camel"]
+word_list = hangman_words.word_list
 chosen_word = random.choice(word_list)
 word_length = len(chosen_word)
-
-#TODO-1: - Create a variable called 'lives' to keep track of the number of lives left. 
-#Set 'lives' to equal 6.
 
 #Testing code
 print(f'Pssst, the solution is {chosen_word}.')
@@ -82,13 +27,18 @@ while not end_of_game:
     isChar = False
     for position in range(word_length):
         letter = chosen_word[position]
-        # print(f"Current position: {position}\n Current letter: {letter}\n Guessed letter: {guess}")
+
+        
         if letter == guess:
-            display[position] = letter
+            if display[position] != letter:
+              display[position] = letter
+            else:
+              print("You already used this letter")
             isChar = True
 
     if isChar == False:
       print(stages.pop())
+      print(f"You guessed a letter {guess}, it's not in the word. You lost 1 life out of {len(stages)}")
     #TODO-2: - If guess is not a letter in the chosen_word,
     #Then reduce 'lives' by 1. 
     #If lives goes down to 0 then the game should stop and it should print "You lose."
@@ -105,5 +55,3 @@ while not end_of_game:
       end_of_game = True
       print("You lost")
 
-
-    #TODO-3: - print the ASCII art from 'stages' that corresponds to the current number of 'lives' the user has remaining.
